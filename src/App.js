96aa002b6a5d0e13559033f87dff807c46c6e1b9
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-08-08 21:09:20 -0300

    hook useStatte

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? "reverse" : "";
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h2>Contador: {counter}</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" onClick={handleClick}>
+            Reverse
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <button type="button" onClick={handleIncrement}>
+            Increment {counter}
+          </button>
+        </p>
       </header>
     </div>
   );
 }
+
+// class App extends Component {
+//   state = {
+//     reserve: false,
+//   };
+
+//   handleClick = () => {
+//     const { reverse } = this.state;
+//     this.setState({ reverse: !reverse });
+//   };
+
+//   render() {
+//     const { reverse } = this.state;
+//     const reverseClass = reverse ? "reverse" : "";
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+//           <button type="button" onClick={this.handleClick}>
+//             Reverse
+//           </button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
