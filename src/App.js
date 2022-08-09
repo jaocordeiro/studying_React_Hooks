8ed67cfb7b0420e3c4d45b9784ab984f66e1a479
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-08-08 21:10:59 -0300

    clean code

--- src/App.js
+++ src/App.js
@@ -35,30 +35,4 @@ function App() {
   );
 }
 
-// class App extends Component {
-//   state = {
-//     reserve: false,
-//   };
-
-//   handleClick = () => {
-//     const { reverse } = this.state;
-//     this.setState({ reverse: !reverse });
-//   };
-
-//   render() {
-//     const { reverse } = this.state;
-//     const reverseClass = reverse ? "reverse" : "";
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-//           <button type="button" onClick={this.handleClick}>
-//             Reverse
-//           </button>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
 export default App;
